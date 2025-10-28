3c2b40b9649a629214e6a702353e8cae948db90b
Vejay Persaud <35679567+VejayPersaud@users.noreply.github.com>
2025-10-28 05:14:24 -0400

    Connected the Go API to Postgres database

--- main.go
+++ main.go
@@ -1,95 +1,89 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"net/http" //HTTP communication tools
+	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
-var storeDatabase = map[string]map[string]interface{}{
-	"1": {
-		"name":         "Awesome Stickers",
-		"revenue":      50000,
-		"total_orders": 342,
-		"active":       true,
-	},
-	"2": {
-		"name":         "Cool Decals",
-		"revenue":      75000,
-		"total_orders": 521,
-		"active":       true,
-	},
-	"3": {
-		"name":         "Vintage Prints",
-		"revenue":      30000,
-		"total_orders": 189,
-		"active":       false,
-	},
+var db *sql.DB
+
+func main() {
+	var err error
+
+	// Connect to Postgres
+	fmt.Println("Connecting to Postgres...")
+	connStr := "host=localhost port=5432 user=postgres password=postgres dbname=sticker_mule_practice sslmode=disable"
+
+	//sql.Open generates the expensive connection from the connStr info and an error which will be nil if everything connects
+	db, err = sql.Open("postgres", connStr)
+
+	//if the error is not an all clear nil then we failed to connect
+	if err != nil {
+		fmt.Println("Failed to connect:", err)
+		return
+	}
+	defer db.Close()
+
+	//Test connection
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Cannot reach database:", err)
+		return
+	}
+	fmt.Println("Connected to Postgres!\n")
+
+	//Register endpoints
+	fmt.Println("Registering endpoints")
+	http.HandleFunc("/health", healthCheck)
+	http.HandleFunc("/store", getStoreInfo)
+
+	fmt.Println("Server listening on http://localhost:8080")
+	fmt.Println("Waiting for requests...\n")
+	http.ListenAndServe(":8080", nil)
 }
 
 func healthCheck(w http.ResponseWriter, r *http.Request) {
-	// TODO: You'll implement this
-	fmt.Println("healthCheck function was called!")
-	fmt.Println("Request Details: ")
-	fmt.Println("  - Method:", r.Method)
-	fmt.Println("  -Path:", r.URL.Path)
-	fmt.Println("  -From:", r.RemoteAddr)
-
-	fmt.Println("Sending response: OK")
+	fmt.Println("/health called")
 	w.Write([]byte("OK"))
-	fmt.Println("Response sent!\n")
-
 }
 
 func getStoreInfo(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("getStoreInfo function was called!")
-	fmt.Println("  - Method:", r.Method)
-	fmt.Println("  -Path:", r.URL.Path)
-	fmt.Println("  -From:", r.RemoteAddr)
-
-	//Get the store ID from the URL
-	//Example: /store?id=123
+	fmt.Println("/store called")
 
 	storeID := r.URL.Query().Get("id")
 	if storeID == "" {
 		storeID = "1"
 	}
-	fmt.Println("Store ID:", storeID)
+	fmt.Println("Looking up store ID:", storeID)
 
-	//Look up store by id in our database
-	storeData, exists := storeDatabase[storeID]
-	if !exists {
-		fmt.Println("Store not found!")
+	//Query Postgress
+	var name string
+	var revenue, totalOrders int
+	var active bool
+
+	query := "SELECT name, revenue, total_orders, active FROM stores WHERE id = $1"
+	err := db.QueryRow(query, storeID).Scan(&name, &revenue, &totalOrders, &active)
+
+	if err == sql.ErrNoRows {
+		fmt.Println("Store not found\n")
 		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte(`{"error": "Store not found"}`))
 		return
+	} else if err != nil {
+		fmt.Println("Database error:", err, "\n")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error": "Database error"}`))
+		return
 	}
 
-	//Building response 1
 	response := fmt.Sprintf(
-		`{"store_id": "%s", "name": "%s", "revenue": %v, "total_orders": %v, "active": %v }`,
-		storeID,
-		storeData["name"],
-		storeData["revenue"],
-		storeData["total_orders"],
-		storeData["active"],
+		`{"store_id": "%s", "name": "%s", "revenue": %d, "total_orders": %d, "active": %t}`,
+		storeID, name, revenue, totalOrders, active,
 	)
 
-	fmt.Println("Sending store data:", response)
+	fmt.Println("Sending:", response, "\n")
 	w.Write([]byte(response))
-	fmt.Println("Store data sent!\n")
-
-}
-
-func main() {
-	fmt.Println("Server is starting...")
-	fmt.Println("Registering /health endpoint")
-	http.HandleFunc("/health", healthCheck)
-
-	fmt.Println("Registering /store endpoint")
-	http.HandleFunc("/store", getStoreInfo)
-
-	fmt.Println("Listening on http://localhost:8080")
-	fmt.Println("Try visiting: http://localhost:8080/health")
-	fmt.Println("Waiting for requests...\n")
-	http.ListenAndServe(":8080", nil)
 }
